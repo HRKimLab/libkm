9079308123e04d7a52fcf6ebba8b942e897a388a
HyungGoo Kim <hkim07@g.harvard.edu>
2020-09-14 15:21:38 -0400

    routine update

--- libutil/ginfo2grp.m
+++ libutil/ginfo2grp.m
@@ -11,6 +11,6 @@ assert(size(ginfo.grp,1) == size(ginfo.grp_idx,1));
 assert((size(ginfo.grp,1) == size(trial_start,1)) || numel(trial_start) == 1);
 assert(length(ginfo.grp_label) == size(ginfo.grp,2) || length(ginfo.grp_label)==0);
 assert(size(ginfo.unq_grp, 1) == length(ginfo.unq_grp_label) );
-assert( nunique(ginfo.grp_idx) == length(ginfo.unq_grp_label) );
+assert( nunique(ginfo.grp_idx) == length(ginfo.unq_grp_label), '# of unique values in grp_idx does not match unq_grp_label');
 % OK. ginfo passed sanity check.
 grp = ginfo.grp_idx;
